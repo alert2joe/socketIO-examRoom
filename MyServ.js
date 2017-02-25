f91502d22391c05d39ab083a67766c08101e67a7
alert2joe <alert2joe@gmail.com>
2017-02-26 00:03:56 +0800

    init

---
+++ MyServ.js
@@ -0,0 +1,113 @@
+function MyServ(socket,roomList){
+    this.roomList = roomList;
+    this.socket = socket;
+    this.startTime= new Date().getTime()
+    this.heartBetCount = 0;
+    this.isAdmin;
+    this.user;
+    this.init=() =>{
+        this.emitHeartBet();
+        this.waitForGameStart();
+        this.roomLogic();
+    };
+
+        this.roomLogic= () => {
+            var that = this;
+            this.socket.on('join', function (roomID,user,isAdmin) {
+                that.user = user;
+                that.roomID = roomID;
+                that.isAdmin = isAdmin;
+     
+                if (!that.roomList[roomID]) {
+                    that.roomList[roomID] = {info:{
+                        roomID:roomID,
+                        gameStartTime:''
+                    },adminSocket:'',user:[]};
+                }
+
+                if(isAdmin){
+                    that.roomList[roomID].adminSocket = that.socket;
+                }
+                
+                that.roomList[roomID].user.push(user);
+
+
+                that.socket.join(roomID); 
+            
+                if(that.roomList[roomID].adminSocket){
+                    var adminSocket = that.roomList[roomID].adminSocket
+                    adminSocket.emit('studentJoinRoom', user  ,that.roomList[roomID].user); 
+                }
+                that.socket.emit('gameInfo', that.roomList[roomID]['info']); 
+
+                console.log(user + ' join ' + roomID);
+            });
+
+
+ 
+            this.socket.on('getRoomInfo', function () {
+                var room = that.roomList[that.roomID];
+              
+                room.adminSocket.emit('RoomInfo', {
+                    abc:123,
+                    user:room.user,
+                    info:room.info
+                });
+            });
+
+            this.socket.on('leave', function () {
+                that.socket.emit('disconnect');
+            });
+
+            this.socket.on('disconnect', function () {
+                if(!that.roomID){
+                    return false;
+                }
+                var room = that.roomList[that.roomID];
+                var index = room.user.indexOf(that.user);
+                if (index !== -1) {
+                    room.user.splice(index, 1);
+                }
+
+                that.socket.leave(that.roomID);
+                if(room.adminSocket){
+                    room.adminSocket.emit('adminMsg', that.user + ' Quit',that.roomID);
+                }
+                console.log(that.user + '退出了' + that.roomID);
+            });
+        }
+
+    this.emitHeartBet= () => {
+            
+             setInterval(function(){
+                 var timeStamp = new Date().getTime();
+                 var duration =  timeStamp -this.startTime;
+                 
+                 this.socket.emit('HeartBet', {
+                     timeStamp:timeStamp,
+                      duration : duration,
+                      heartBetCount : this.heartBetCount++
+                });
+                 }.bind(this), 1000);
+    }
+
+    this.waitForGameStart=()=>{
+        var that = this;
+        this.socket.on('gameStart', function () {
+            var room = that.roomList[that.roomID];
+            var gameStartTime = new Date().getTime();
+            room.info.gameStartTime = gameStartTime;
+            that.socket.to(that.roomID).emit('gameStart', 
+                {
+                    gameStartTime:gameStartTime
+                    }); 
+ 
+           }); 
+    }
+
+
+}
+
+module.exports = MyServ;
+
+  
